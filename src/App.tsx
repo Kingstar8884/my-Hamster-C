308a379faa3cbf9aa6ac772bf2a880b021b9e5ab
Kingstar8884 <115646493+Kingstar8884@users.noreply.github.com>
2024-08-23 23:04:22 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -224,7 +224,7 @@ const App: React.FC = () => {
       <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
         <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
           <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Exchange</p>
+          <p className="mt-1">Exchangess</p>
         </div>
         <div className="text-center text-[#85827d] w-1/5">
           <Mine className="w-8 h-8 mx-auto" />
